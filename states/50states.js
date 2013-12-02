f41fe14b331ce67be1a9c7f7c919207a33fa4d5f
Svilen Kanev <skanev@eecs.harvard.edu>
2013-12-02 00:48:24 -0500

    Track js too.

---
+++ states/50states.js
@@ -0,0 +1,90 @@
+function drawStatesMap() {
+
+  var statesFile = "https://dl.dropboxusercontent.com/s/t4h6uol5jgudo3e/us.json";
+  var statesNamesFile = "https://dl.dropboxusercontent.com/s/h8pts8fkzgarajg/state_names.json";
+
+  function nameByID(id, names) {
+    for (i in names) {
+      if (names[i].id == id)
+        return names[i];
+    }
+    return null;
+  }
+  function ranByState(state, ran) {
+    for (i in ran) {
+      if (ran[i].state == state)
+        return ran[i];
+    }
+    return null;
+  }
+  function ranByID(id, names, ran) {
+    var state = nameByID(id, names);
+    if (state == null)
+      return false;
+    return ranByState(state.state, ran);
+  }
+  function hasRun(id, names, ran) {
+    var state = nameByID(id, names);
+    if (state == null)
+      return false;
+    return (ranByState(state.state, ran) != null);
+  }
+
+  var path = d3.geo.path();
+
+  var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d, names, ran) {
+      return "<span class='tip-heading'>" + nameByID(d.id, names).name +"</span> <br/> <br/> "+ ranByID(d.id, names, ran).text;
+    })
+
+  var container = d3.select(".states-container");
+  var dataFile = container.attr("data");
+
+  var svg = container.append("svg")
+      .attr("id", "map")
+      .attr("viewBox", "0 0 960 500")
+      .attr("width", "100%");
+
+  svg.call(tip);
+
+  function json(path, callback) {
+    d3.json(path, function(json_) {
+      json_ ? callback(null, json_) : callback("error", null);
+    });
+  }
+
+  queue()
+    .defer(json, statesFile)
+    .defer(json, statesNamesFile)
+    .defer(json, dataFile)
+    .await(function(error, us, names, ran) {
+      if (error) console.error(error)
+
+      svg.append("path")
+          .datum(topojson.feature(us, us.objects.land))
+          .attr("class", "land")
+          .attr("d", path);
+
+      svg.selectAll(".state")
+          .data(topojson.feature(us, us.objects.states).features)
+          .enter().append("path")
+          .attr("class", function(d) {
+            ran_ = ranByID(d.id, names, ran);
+            if (ran_ == null) return "state";
+            if (ran_.text == "TBD") return "state tbd";
+            return "state ran";
+          })
+          .attr("d", path)
+          .on('mouseover', function(d) {
+            if (hasRun(d.id, names, ran))
+              tip.show(d, names, ran);
+          })
+          .on('mouseout', function(d) {
+            if (hasRun(d.id, names, ran))
+              tip.hide(d, names, ran);
+          });
+    });
+
+}
---
+++ states/50statesLoader.js
@@ -0,0 +1,42 @@
+(function() {
+    
+    var mapBloggerPath = "http://s-kanev.github.io/runstats/states/";
+    //var mapBloggerPath = "http://localhost:8088/"
+    var mapBloggerJS = mapBloggerPath + "50states.js";
+    var mapBloggerCSS = mapBloggerPath + "50states.css";
+    var d3TipJS = "http://labratrevenge.com/d3-tip/javascripts/d3.tip.min.js";
+
+    // Initialize the main script.
+    try {
+        drawStatesMap();
+    } catch(e) {
+        var scriptId = 'mapPrinter';
+        if (document.getElementById(scriptId) === null) {
+            /* Append CSS to head */
+            var cssElem = document.createElement('link');
+            cssElem.rel = 'stylesheet';
+            cssElem.type = 'text/css';
+            cssElem.href = mapBloggerCSS;
+            var theHead = document.getElementsByTagName('head')[0];
+            theHead.appendChild(cssElem);
+
+            /* Append main script and register onload */
+            var elem = document.createElement('script');
+            elem.id = scriptId;
+            elem.onload = function() {
+                /* Ok, I'm sure this can generalize. */
+                var newElem = document.createElement('script');
+                newElem.id = "mapPrinter2";
+                newElem.onload = function() {
+                    drawStatesMap();
+                }
+                newElem.src = mapBloggerJS;
+                var theBody2 = document.getElementsByTagName('body')[0];
+                theBody2.appendChild(newElem);
+            }
+            elem.src = d3TipJS;
+            var theBody = document.getElementsByTagName('body')[0];
+            theBody.appendChild(elem);
+        }
+    }
+})();
